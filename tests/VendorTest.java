edf8f0bae76a780e819ca86a4fb0ba378316aa02
acgreve03 <acgreve.home@icloud.com>
2024-11-07 10:09:58 -0600

    added a test case to validate that I can add money to a vendor.

--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -1,12 +1,28 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VendorTest {
 
+    static Vending v;
+
+    @BeforeAll
+    public static void setup(){
+        v = new Vending(1, 1);
+    }
+
     @Test
     void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    void addMoneyTest(){
+        v.addMoney(2.0);
+        assertEquals(2.0, v.getBalance());
+    }
+
 
 }
